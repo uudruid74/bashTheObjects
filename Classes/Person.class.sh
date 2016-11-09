695a2df47ea5d0f62cfeae35f2e6b50643cc83c4
Evan Langlois <evan@eddon.systems>
2016-11-08 21:38:49 -0600

    Missing files

---
+++ Classes/Person.class.sh
@@ -0,0 +1,21 @@
+
+class Person
+        public show
+        public set
+        public Name
+        public Age
+        public Sex
+        inst var Name "Saranyan"
+        inst var Age 10
+        inst var Sex "Male"
+
+Person::Person() { :; }
+Person::set() { :; }
+Person::Name() { println $Name; }
+Person::Age() { println $Age; }
+Person::Sex() { println $Sex; }
+Person::show() {
+	Person::Name
+	Person::Age
+	Person::Sex
+}
---
+++ testperson.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source static/oop.lib.sh
+
+import Person
+
+new Person Christy Name: "Christy" Age: 21 Sex: "female"
+new Person Evan Name: "Evan" Age: 41 Sex: "male"
+
+println "$(Evan.Name) is a $(Evan.Sex) aged $(Evan.Age)"
+println "$(Christy.Name) is a $(Christy.Sex) aged $(Christy.Age)"
+println "Stats for Evan ..."
+Evan.show
+
+assert 'kindof Person Evan'
+assert '[ $Evan = $Evan ]'
+assert 'kindof Person Christy'
+assert '[ $Evan = $Christy ]'
+
